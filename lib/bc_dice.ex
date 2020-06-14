fc19378cc9f772fbeaf339fb691e41d092334798
epona <490472721@qq.com>
2020-06-14 22:05:17 +0900

    init

---
+++ lib/bc_dice.ex
@@ -0,0 +1,18 @@
+defmodule BCDice do
+  @moduledoc """
+  Documentation for `BCDice`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BCDice.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bc_dice_test.exs
@@ -0,0 +1,8 @@
+defmodule BCDiceTest do
+  use ExUnit.Case
+  doctest BCDice
+
+  test "greets the world" do
+    assert BCDice.hello() == :world
+  end
+end
